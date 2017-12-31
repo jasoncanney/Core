d9c64385d79931fdbf8f7aa2f844fd99acf4f1a8
Jason Canney <jaycanney@gmail.com>
2017-12-30 17:31:24 -0700

    Working on finding and fixing the datetime issue with preference

--- src/test/java/com/canopyaudience/model/services/preferenceservice/PreferenceSvcHibernateImplTest.java
+++ src/test/java/com/canopyaudience/model/services/preferenceservice/PreferenceSvcHibernateImplTest.java
@@ -6,6 +6,7 @@
 package com.canopyaudience.model.services.preferenceservice;
 
 import com.canopyaudience.model.domain.preference;
+import com.canopyaudience.model.services.exception.PreferenceException;
 import java.security.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -37,15 +38,12 @@ public class PreferenceSvcHibernateImplTest extends TestCase{
 		super.setUp();
         
                 
-    String date_1 = "2011-01-18 23:59:01.0"; 
-    String date_2 = "2017-01-18 23:59:01.0";
-    SimpleDateFormat dt = new SimpleDateFormat("yyyyy-mm-dd hh:mm:ss"); 
-    Date d1 = dt.parse(date_1);
-    Date d2 = dt.parse(date_2);
+        long date_1 = System.currentTimeMillis(); 
+        long date_2 = System.currentTimeMillis();
             
-        preference1 = new preference (27,1,d1,6,7,8675);
-        preference2 = new preference (28,1,d1,12,69,8675);
-        preference3 = new preference (88,0,d2,6,7,8675);      
+        preference1 = new preference (1,date_1,67500,6,8675);
+        preference2 = new preference (1,date_1,67500,12,8675);
+        preference3 = new preference (0,date_2,6,7,8675);      
 
         }
         /*
@@ -75,34 +73,36 @@ public class PreferenceSvcHibernateImplTest extends TestCase{
     public void test2GetAPreference() throws Exception {
         log.info ("PreferenceSvcHibernateImplTest - test2GetAPreference");
         PreferenceSvcHibernateImpl instance = new PreferenceSvcHibernateImpl();
-        preference c = instance.getAPreference(preference1.getPreferenceId());
+        preference c = instance.getAPreference(preference1.getAdvertisementID());
         log.info(c.toString());
-        int expResult = 8675;
-        assertEquals(expResult, preference1.getConsumerId()); 
-        preference2.setConsumerId(preference1.getConsumerId());
+        int expResult = 67500;
+        assertEquals(expResult, preference1.getAdvertisementID()); 
+        // preference2.setConsumerId(preference1.getConsumerId());
     }*/
  
     /**
      * Test of updatePreference method, of class PreferenceSvcHibernateImpl.
      *//*
     @Test
-    public void test3UpdatePreference() {
+    public void test3UpdatePreference() throws PreferenceException, ClassNotFoundException {
         log.info ("PreferenceSvcHibernateImplTest - test3UpdatePreference");
         PreferenceSvcHibernateImpl instance = new PreferenceSvcHibernateImpl();
         boolean expResult = true;
-        boolean result = instance.updatePreference(preference2);  // adimpression2 is a pre-updated adimpression object for adimpression1
+        preference c = instance.getAPreference(preference1.getAdvertisementID());
+        c.setCouponID(preference2.getCouponID());
+        boolean result = instance.updatePreference(c);  // adimpression2 is a pre-updated adimpression object for adimpression1
         assertEquals(expResult, result);    
-    }*/
+    }*?
     /**
      * Test of deletePreference method, of class PreferenceSvcHibernateImpl.
      *//*
     @Test
-    public void test4DeletePreference() {
+    public void test4DeletePreference() throws PreferenceException, ClassNotFoundException {
         log.info ("PreferenceSvcHibernateImplTest - test4DeletePreference");
         PreferenceSvcHibernateImpl instance = new PreferenceSvcHibernateImpl();
         boolean expResult = true;
-        boolean result = instance.deletePreference(preference2);
+        preference c = instance.getAPreference(preference1.getAdvertisementID());
+        boolean result = instance.deletePreference(c);
         assertEquals(expResult, result); 
-    }
-    */
+    }*/
 }
